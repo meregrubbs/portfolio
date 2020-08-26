b3319b0b1b9d48b93c6d0deeb0fb3e5271a70b59
Meredith Grubbs <meregrubbs@gmail.com>
2020-08-25 19:05:50 -0600

    website reset + working on react/material ui

--- app.js
+++ app.js
@@ -1,72 +1,13 @@
-//
-// HAMBURGER ICON DISPLAY
-//
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
 
-const nav = document.querySelector('#navbar');
-const hamburger = document.querySelector('#hamburger');
-
-// on startup, run onResize
-onResize();
-
-// listen for screen resize
-window.addEventListener('resize', onResize);
-
-function onResize() {
-    // determine screen width
-    const screenWidth = window.innerWidth;
-
-    // if screen width is less than 1024
-    if (screenWidth < 1024) {
-        // hide nav
-        nav.style.display = 'none';
-
-        // show hamburger
-        hamburger.style.display = 'inline';
-    }
-    else {
-        // show nav
-        nav.style.display = 'inline';
-
-        // hide hamburger
-        hamburger.style.display = 'none';
-    }
+function App() {
+  return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
 }
 
-//
-// HAMBURGER MENU TOGGLE
-//
-
-const hamburgerIcon = document.querySelector('#hamburger-icon');
-const hamburgerNav = document.querySelector('#hamburger-nav');
-
-// event listener
-hamburgerIcon.addEventListener('click', hamburgerMenu);
-
-function hamburgerMenu() {
-    console.log('clicked');
-
-    console.log(hamburgerNav);
-
-    // hide menu
-    if (hamburgerNav.innerHTML !== '') {
-        // hide html
-        hamburgerNav.innerHTML = '';
-
-        // hide style
-        hamburger.style.backgroundColor = 'transparent';
-
-    // show menu    
-    } else {
-        // show html
-        hamburgerNav.innerHTML = `
-            <a class="active" href="index.html">Home</a>
-            <a href="#">Portfolio</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>`;
-
-        // show style
-        hamburger.style.backgroundColor = 'rgba(0, 0, 0, .3)';
-    }
-}
-
-
+ReactDOM.render(<App />, document.querySelector('#app'));
