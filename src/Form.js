d9a80c1e9dd93504f02b749bfb643f6ea4a55b03
Meredith Grubbs <meregrubbs@gmail.com>
2020-09-13 15:17:42 -0600

    again

--- src/Form.js
+++ src/Form.js
@@ -6,9 +6,9 @@ export default class extends React.Component {
   constructor(props) {
     super(props);
     this.state = { feedback: '', name: '', email: '' };
-    this.handleMessageChange = this.handleChange.bind(this);
-    this.handleEmailChange = this.handleChange.bind(this);
-    this.handleNameChange = this.handleChange.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
