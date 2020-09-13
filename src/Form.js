41cec23f0b289d31de97fa7a5c6b364a56040b06
Meredith Grubbs <meregrubbs@gmail.com>
2020-09-13 16:23:42 -0600

    form is working!

--- src/Form.js
+++ src/Form.js
@@ -5,6 +5,8 @@ export default function Form() {
   function sendEmail(e) {
     e.preventDefault();
 
+    console.log(e.target);
+
     emailjs
       .sendForm(
         'service_nns7oa9',
@@ -14,7 +16,27 @@ export default function Form() {
       )
       .then(
         result => {
-          console.log(result.text);
+          // clear form
+          const name = document.getElementById('name');
+          const email = document.getElementById('email');
+          const message = document.getElementById('message');
+
+          name.value = '';
+          email.value = '';
+          message.value = '';
+
+          // sent message
+          const submit = document.getElementById('submit');
+
+          submit.value = 'Message Sent!';
+          submit.style.fontWeight = 'bold';
+          submit.style.borderColor = 'green';
+
+          setTimeout(function() {
+            submit.value = 'Send';
+            submit.style.fontWeight = '500';
+            submit.style.borderColor = '#c777da';
+          }, 3000);
         },
         error => {
           console.log(error.text);
@@ -26,12 +48,12 @@ export default function Form() {
     <form className='contact-form' onSubmit={sendEmail}>
       <input type='hidden' name='contact_number' />
       <label>Name</label>
-      <input type='text' name='user_name' />
+      <input type='text' name='user_name' id='name' />
       <label>Email</label>
-      <input type='email' name='user_email' />
+      <input type='email' name='user_email' id='email' />
       <label>Message</label>
-      <textarea name='message' />
-      <input class='btn--submit' type='submit' value='Send' />
+      <textarea name='message' id='message' />
+      <input className='btn--submit' type='submit' value='Send' id='submit' />
     </form>
   );
 }
