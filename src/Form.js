4218071deba76b1c3872eb8e961a103ba35e9caf
Meredith Grubbs <meregrubbs@gmail.com>
2020-09-13 16:47:24 -0600

    form messages work!

--- src/Form.js
+++ src/Form.js
@@ -5,47 +5,65 @@ export default function Form() {
   function sendEmail(e) {
     e.preventDefault();
 
-    console.log(e.target);
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const message = document.getElementById('message');
+    const messages = document.querySelector('.messages');
 
-    emailjs
-      .sendForm(
-        'service_nns7oa9',
-        'template_gx9h187',
-        e.target,
-        'user_FQvCwBFGhZjkHcWbSevt9'
-      )
-      .then(
-        result => {
-          // clear form
-          const name = document.getElementById('name');
-          const email = document.getElementById('email');
-          const message = document.getElementById('message');
+    if (name.value !== '' && email.value !== '' && message.value !== '') {
+      emailjs
+        .sendForm(
+          'service_nns7oa9',
+          'template_gx9h187',
+          e.target,
+          'user_FQvCwBFGhZjkHcWbSevt9'
+        )
+        .then(
+          result => {
+            // clear form
+            name.value = '';
+            email.value = '';
+            message.value = '';
 
-          name.value = '';
-          email.value = '';
-          message.value = '';
+            // sent message
+            const submit = document.getElementById('submit');
 
-          // sent message
-          const submit = document.getElementById('submit');
+            submit.value = 'Message Sent!';
+            submit.style.fontWeight = 'bold';
+            submit.style.borderColor = 'green';
 
-          submit.value = 'Message Sent!';
-          submit.style.fontWeight = 'bold';
-          submit.style.borderColor = 'green';
+            setTimeout(function() {
+              submit.value = 'Send';
+              submit.style.fontWeight = '500';
+              submit.style.borderColor = '#c777da';
+            }, 3000);
+          },
+          error => {
+            console.log(error.text);
+          }
+        );
+    } else {
+      messages.innerHTML =
+        '<p style="margin: 0;">Please fill in all fields.</p>';
+      messages.style.backgroundColor = 'rgba(247,240,250,1';
+      messages.style.color = 'red';
+      messages.style.marginBottom = '1em';
+      messages.style.padding = '.5em';
+      messages.style.borderRadius = '4px';
+      messages.style.width = '36%';
 
-          setTimeout(function() {
-            submit.value = 'Send';
-            submit.style.fontWeight = '500';
-            submit.style.borderColor = '#c777da';
-          }, 3000);
-        },
-        error => {
-          console.log(error.text);
-        }
-      );
+      setTimeout(function() {
+        messages.innerHTML = '';
+        messages.style.backgroundColor = 'transparent';
+        messages.style.padding = '0';
+        messages.style.margin = '0';
+      }, 3000);
+    }
   }
 
   return (
     <form className='contact-form' onSubmit={sendEmail}>
+      <div className='messages'></div>
       <input type='hidden' name='contact_number' />
       <label>Name</label>
       <input type='text' name='user_name' id='name' />
